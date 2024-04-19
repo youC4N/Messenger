bfa314cc1b02e9f0825075091832b8f78f292474
Egor Malyhin <workmalyhin@gmail.com>
2024-04-19 19:29:37 +0300

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Егор Малыгин on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
