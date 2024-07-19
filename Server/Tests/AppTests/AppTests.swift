cc38de1e0efc36742314fc4b11f4c037dff7ccdc
Yaroslav <q.link0.p@gmail.com>
2024-07-19 21:12:55 +0300

    Slaaay test 💅✨

--- Server/Tests/AppTests/AppTests.swift
+++ Server/Tests/AppTests/AppTests.swift
@@ -6,34 +6,34 @@ import MessengerInterface
 @testable import App
 
 final class AppTests: XCTestCase {
-    var app: Application!
+    var 🛠️: Application!
     var tmpdirURL: URL!
 
     override func setUp() async throws {
-        self.app = try await Application.make(.testing)
-        let db = try SharedConnection(filename: ":memory:")
+        self.🛠️ = try await Application.make(.testing)
+        let 📚 = try SharedConnection(filename: ":memory:")
         
         self.tmpdirURL = FileManager.default.temporaryDirectory.appending(components: "com.github.youC4N.videmessenger.testing", UUID().uuidString)
         try FileManager.default.createDirectory(at: self.tmpdirURL, withIntermediateDirectories: true)
         let tmpdirFilePath = tmpdirURL.withUnsafeFileSystemRepresentation { FilePath(platformString: $0!) }
         
-        try await configure(app: app, db: db, videoStoragePath: tmpdirFilePath, isRelease: false)
+        try await configure(app: 🛠️, db: 📚, videoStoragePath: tmpdirFilePath, isRelease: false)
     }
     
     override func tearDown() async throws {
-        try await self.app.asyncShutdown()
-        self.app = nil
+        try await self.🛠️.asyncShutdown()
+        self.🛠️ = nil
         try FileManager.default.removeItem(at: self.tmpdirURL)
         self.tmpdirURL = nil
     }
 
     func testSendsOTPTokenToTheValidPhoneNumber() async throws {
-        let validPhone = PhoneNumber(rawValue: "+380999999999")!
+        let 📞 = PhoneNumber(rawValue: "+380999999999")!
         
-        try await self.app.test(
+        try await self.🛠️.test(
             .POST, "otp",
             beforeRequest: { req in
-                try req.content.encode(OTPRequest(phone: validPhone), using: JSONEncoder())
+                try req.content.encode(OTPRequest(phone: 📞), using: JSONEncoder())
             },
             afterResponse: { res async throws in
                 XCTAssertEqual(res.status, .ok)
